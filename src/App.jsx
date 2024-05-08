1d3401197d8fe54782f88e2329f661675fa3e953
heping <416207848@qq.com>
2024-05-08 12:01:57 +0800

    react初始化模板

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 const App = () => {
   return (
-    <div className=''>App</div>
-  )
-}
+    <div className="container bg-slate-500 mx-auto h-fit border border-red-100">
+      <span className=" text-4xl">组件1</span>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+//配置路径别名
+import path from "path";
+const __dirname = path.resolve();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "src"),
+      },
+    ],
+  },
+});
